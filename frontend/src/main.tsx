8fbc6d08c88d8fa26a48dffcc9921329bdc968f8
alfonsograziano <alfonsohack0019@gmail.com>
2025-01-03 00:04:10 +0100

    initialized node-based editor

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Edit from "./pages/Edit.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { BrowserRouter, Routes, Route } from "react-router";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/edit" element={<Edit />} />
+
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
