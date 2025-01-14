04fe82be586595d0d0c3ef9c8d50beee1bda4b0f
alfonsograziano <alfonsohack0019@gmail.com>
2025-01-14 18:45:52 +0100

    new conditional run test

--- backend/core/__tests__/engine.test.ts
+++ backend/core/__tests__/engine.test.ts
@@ -7,8 +7,9 @@ import {
   mockBasicWorkflowTemplate,
   mockWorkflowTemplateWithConditionalPlugin,
   buildMockData,
+  createStepRunId,
 } from "./mockData";
-import { WorkflowStatus } from "@prisma/client";
+import { StepRunStatus, WorkflowStatus } from "@prisma/client";
 import { initPluginsRegistry } from "../pluginRegistry";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -55,5 +56,36 @@ describe("WorkflowEngine", () => {
     expect(
       repository.getMockData().workflowRuns["run1WithConditionals"].status
     ).toBe(WorkflowStatus.COMPLETED);
+  });
+
+  it("should execute a the right step in a conditional statement", async () => {
+    const repository = new WorkflowRunRepositoryMock(buildMockData());
+    const runId = "run1WithConditionals";
+    const engine = new WorkflowEngine({
+      workflow: mockWorkflowTemplateWithConditionalPlugin,
+      repository: repository as unknown as WorkflowRunRepository,
+      runId,
+    });
+
+    await engine.execute();
+
+    expect(
+      repository.getMockData().workflowRuns["run1WithConditionals"].status
+    ).toBe(WorkflowStatus.COMPLETED);
+
+    const expectedStepToRun = "step2";
+    const epxectedStepToNotRun = "step3";
+
+    expect(
+      repository.getMockData().stepRuns[
+        createStepRunId(runId, expectedStepToRun)
+      ].status
+    ).toBe(StepRunStatus.COMPLETED);
+
+    expect(
+      repository.getMockData().stepRuns[
+        createStepRunId(runId, epxectedStepToNotRun)
+      ]
+    ).toBe(undefined);
   });
 });
